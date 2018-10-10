3875e432df17606d6d2a08f5ae605923044970a8
Amil Masic <amil.masic@gmail.com>
2018-10-10 21:46:30 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,9 +6,15 @@ def create_project_hash
   # write your code here
 
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  # binding.pry
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+
+  projects
 end
 
  create_project_hash
